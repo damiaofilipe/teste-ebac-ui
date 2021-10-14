3dc00221cce390d5f561743789a93c441ba321b5
Damião Filipe <damiaofilipe@hotmail.com>
2021-10-14 19:48:26 -0300

    alterações de teste automatizados

--- cypress/integration/Pre-cadastro.spec.js
+++ cypress/integration/Pre-cadastro.spec.js
@@ -9,13 +9,16 @@ describe('Funcionalidade Pré cadastro', () => {
     });
     
     it('Deve completar o pré cadastro com sucesso', () => {
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker) 
 
-        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('senha@senha.com')
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'modificados com sucesso')
--- cypress/integration/Produtos.spec.js
+++ cypress/integration/Produtos.spec.js
@@ -10,11 +10,28 @@ describe('Funcionalidade Pagina de produtos', () => {
 
     it('Deve selecionar um produto da lista', () => {
 
-        cy.get('[class="image-hover unveil-image"]')
+        cy.get('[Class="product-block grid"]')
             //.first()
             //.last()
             //.eq(5)
             .contains('Ingrid Running Jacket')
             .click()
+
+    });
+
+    it.only('Deve adicionar um produto no carrinho', () => {
+        cy.get('[Class="product-block grid"]')
+            .contains('Ingrid Running Jacket')
+            .click()
+            cy.get('.button-variable-item-L')
+            .click()
+            cy.get('.button-variable-item-Orange')
+            .click()
+            cy.get('.input-text').clear().type(6)
+            cy.get('.single_add_to_cart_button')
+            .click()
+
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 6)
+
     });
 });
